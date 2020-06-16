8e1c2f6b9add0195720326bbbd903d16f4ae5ace
Ryder Stanley <ryderstanley2007@gmail.com>
2020-06-15 20:44:11 -0600

    started if statement and created variables

--- main.c
+++ main.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main() {
+
+    char username[];
+    char password[];
+    char los[];
+    char si[] = "sign in";
+    char li[] = "login";
+
+    printf("Type login or sign in: \n");
+    scanf("%s", los);
+
+    if (los == si) {
+        printf("Enter a Username: ");
